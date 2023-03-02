5b8c9f8843dfcbdb0648e9aa45333fe2ac03779e
Rusu Cristian 1E3 <39808975+Plutonium8T8@users.noreply.github.com>
2023-03-03 00:48:33 +0200

    dev v0.2.2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,15 +5,5 @@ using System;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,10 +6,6 @@ using Utils;
 
 public class GameController : MonoBehaviour
 {
-    // [SerializeField] private Transform selectionAreaTransform;
-
-    // [SerializeField] public CameraController cameraController;
-
     private Vector2 _startPosition;
 
     private List<Unit> selectedEntitiesList;
@@ -17,41 +13,16 @@ public class GameController : MonoBehaviour
     private void Awake()
     {
         selectedEntitiesList = new List<Unit>();
-
-        // selectionAreaTransform.gameObject.SetActive(false);
     }
     private void Update()
     {
-        Debug.Log(1);
-        if (Input.GetMouseButton(0))
-        {
-            Vector2 currentMousePosition = UtilsClass.GetMouseWorldPosition();
-
-            Vector2 lowerLeft = new Vector2(
-                Mathf.Min(_startPosition.x, currentMousePosition.x),
-                Mathf.Min(_startPosition.y, currentMousePosition.y)
-                );
-            Vector2 upperRight = new Vector2(
-                Mathf.Max(_startPosition.x, currentMousePosition.x),
-                Mathf.Max(_startPosition.y, currentMousePosition.y)
-                );
-
-            // selectionAreaTransform.position = lowerLeft;
-
-            // selectionAreaTransform.localScale = upperRight - lowerLeft;
-        }
-
         if (Input.GetMouseButtonDown(0)) // GetMouseButtonDown(LeftClick)
         {
-            // selectionAreaTransform.gameObject.SetActive(true);
-
             _startPosition = UtilsClass.GetMouseWorldPosition();
         }
 
         if (Input.GetMouseButtonUp(0)) // GetMouseButtonUp(LeftClick)
         {
-            // selectionAreaTransform.gameObject.SetActive(false);
-
             Collider2D[] collider2DArray = Physics2D.OverlapAreaAll(_startPosition, UtilsClass.GetMouseWorldPosition());
 
             foreach (Unit unit in selectedEntitiesList)
