399b01b6179ce6b505a89fa6d40ea55b803f6aee
Rusu Cristian 1E3 <39808975+Plutonium8T8@users.noreply.github.com>
2023-03-02 18:03:37 +0200

    dev 0.1.3

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,53 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    [SerializeField]private Camera _camera;
+    [SerializeField] private Camera _camera;
+
+    [SerializeField] public float cameraSpeed = 0.001f;
+
+    [SerializeField] public float cameraMovementRatio = 0.001f;
+
+    private Vector2 _mousePosition;
+
+    private Vector2 cameraMovePosition;
+
+    private void Update()
+    {
+        _mousePosition = _camera.ScreenToViewportPoint(Input.mousePosition);
+        Debug.Log(_mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_mousePosition.x > 0.90 && _camera.transform.position.x <= 30)
+        {
+            cameraMovePosition = _camera.transform.position;
+            cameraMovePosition.x += cameraMovementRatio * Time.deltaTime;
+            _camera.transform.position = cameraMovePosition;
+            _camera.transform.position += new Vector3(_camera.transform.position.x, _camera.transform.position.y, -10);
+        }
+        else if (_mousePosition.x < 0.10 && _camera.transform.position.x >= -30)
+        {
+            cameraMovePosition = _camera.transform.position;
+            cameraMovePosition.x -= cameraMovementRatio * Time.deltaTime;
+            _camera.transform.position = cameraMovePosition;
+            _camera.transform.position += new Vector3(_camera.transform.position.x, _camera.transform.position.y, -10);
+        }
 
 
+        if (_mousePosition.y > 0.90 && _camera.transform.position.y <= 20)
+        {
+            cameraMovePosition = _camera.transform.position;
+            cameraMovePosition.y += cameraMovementRatio * Time.deltaTime;
+            _camera.transform.position = cameraMovePosition;
+            _camera.transform.position += new Vector3(_camera.transform.position.x, _camera.transform.position.y, -10);
+        }
+        else if (_mousePosition.y < 0.10 && _camera.transform.position.y >= -20)
+        {
+            cameraMovePosition = _camera.transform.position;
+            cameraMovePosition.y -= cameraMovementRatio * Time.deltaTime;
+            _camera.transform.position = cameraMovePosition;
+            _camera.transform.position += new Vector3(_camera.transform.position.x, _camera.transform.position.y, -10);
+        }
+    }
 }
